3fb9d1cb6f3cf15c8b3c9791687ee20bfa4b9da1
taylorcarlsen <taylorcarlsen@users.noreply.github.com>
2019-09-13 14:04:44 -0500

    Changed project type class lib

---
+++ Octo.Net/Octo.Net.Data1/Artwork.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Octo.Net.Data
+{
+    class Artwork
+    {
+        public int Id { get; set; }
+        public int GalleryId { get; set; }
+        public string Title { get; set; }
+        public double Price { get; set; }
+        public bool IsCommission { get; set; }
+        public int TagId { get; set; }
+        public int CollectionMessageId { get; set; }
+    }
+}
