0ed54656b58e3f0be2eaad7ececc74fe6864a99a
Mitchell Potrykus <potrykus0507@fvtc.edu>
2019-09-26 14:13:07 -0500

    Started building BL tests

---
+++ Octo.Net/Octo.Net.BL.Test2/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Octo.Net.BL.Test2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Octo.Net.BL.Test2")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d75f30bb-a261-4024-a811-ca3319403506")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Octo.Net/Octo.Net.BL.Test2/utCritique.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Octo.Net.Models;
+using Octo.Net.BL;
+using System.Linq;
+
+namespace Octo.Net.BL.Test
+{
+    [TestClass]
+    public class utCritique
+    {
+
+        [TestMethod]
+        public void InsertTest()
+        {
+            Models.Critique critique = new Octo.Net.Models.Critique();
+            critique.CategoryDescription = "This is a test";
+
+            int result = BL.Critique.Insert(critique);
+            Assert.IsTrue(result > 0);
+        }
+    }
+}
