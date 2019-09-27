6f37636509e3b3ba7e406c93aceb80a6018a29a9
brown4914 <brown4914@fvtc.edu>
2019-09-26 23:12:36 -0500

    Set up user load on admin page.

--- Octo.Net/Octo.Net.UI/Controllers/AdminController.cs
+++ Octo.Net/Octo.Net.UI/Controllers/AdminController.cs
@@ -8,10 +8,21 @@ namespace Octo.Net.UI.Controllers
 {
     public class AdminController : Controller
     {
+        private List<Net.Models.User> _users;
+        private BL.User _user;
+
         // GET: Admin
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult Users()
+        {
+            _user = new BL.User();
+            _users = new List<Net.Models.User>();
+            _users = _user.Load();
+            return View(_users);
         }
     }
 }
--- Octo.Net/Octo.Net.UI/Models/UsersModel.cs
+++ Octo.Net/Octo.Net.UI/Models/UsersModel.cs
@@ -2,10 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Octo.Net.BL;
+using Octo.Net.Models;
+
 
 namespace Octo.Net.UI.Models
 {
-    public class users
+    public class UsersModel
     {
+    
     }
 }
