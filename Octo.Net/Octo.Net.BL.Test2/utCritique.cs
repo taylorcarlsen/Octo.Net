bcbffb8098249738b3b9d7a2f41013ac219f1855
Mitchell Potrykus <potrykus0507@fvtc.edu>
2019-09-27 13:39:35 -0500

    Critique - Insert/Delete/Update Tests

--- Octo.Net/Octo.Net.BL.Test2/utCritique.cs
+++ Octo.Net/Octo.Net.BL.Test2/utCritique.cs
@@ -15,11 +15,44 @@ namespace Octo.Net.BL.Test
         [TestMethod]
         public void InsertTest()
         {
-            //Models.Critique critique = new Octo.Net.Models.Critique();
-            //critique.CategoryDescription = "This is a test";
+            Models.Critique critique = new Octo.Net.Models.Critique();
+            critique.CategoryDescription = "This is a test";
 
-            //int result = BL.Critique.Insert(critique);
-            //Assert.IsTrue(result > 0);
+            Critique blCritique = new Critique();
+            int result = blCritique.Insert(critique);
+
+            Assert.IsTrue(result > 0);
+        }
+
+        public void UpdateTest()
+        {
+            Critique critique = new Critique();
+            List<Models.Critique> critiques = new List<Models.Critique>();
+            //critiques = critiques.Load();
+            Models.Critique row = critiques.Where(m => m.CategoryDescription == "This is a test body of a private critique.").FirstOrDefault();
+
+            row.CategoryDescription = "UPDATED: This is a test body of a private critique.";
+            critique.Update(row);
+
+            List<Models.Critique> updated = new List<Models.Critique>();
+            //updated = critique.Load();
+            Models.Critique updatedCritique = updated.Where(x => x.CategoryDescription == "This is a test body of a private critique.").FirstOrDefault();
+
+            Assert.AreNotEqual(updatedCritique, row);
+        }
+
+        [TestMethod]
+        public void DeleteTest()
+        {
+            Critique critique = new Critique();
+            List<Models.Critique> critiques = new List<Models.Critique>();
+            critiques = critique.Load();
+            Models.Critique row = critiques.Where(x => x.Id == 2).FirstOrDefault();
+            if (row != null)
+            {
+                bool actual = critique.Delete(row.Id);
+                Assert.IsTrue(actual == true);
+            }
         }
     }
 }
