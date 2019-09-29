afa84272af0afe4880ba89afc8471460135cdf65
brown4914 <brown4914@fvtc.edu>
2019-09-29 14:03:02 -0500

    Added post to join controller

--- Octo.Net/Octo.Net.UI/Controllers/JoinController.cs
+++ Octo.Net/Octo.Net.UI/Controllers/JoinController.cs
@@ -11,7 +11,24 @@ namespace Octo.Net.UI.Controllers
         // GET: Join
         public ActionResult Index()
         {
-            return View();
+            BL.User user = new BL.User();
+            return View(user);
         }
+
+        [HttpPost]
+        public ActionResult Index(Net.Models.User user)
+        {
+            try
+            {
+                BL.User bluser = new BL.User();
+                bluser.Insert(user);
+                return RedirectToAction("Index");
+            }
+            catch 
+            {
+                return View(user);   
+            }
+        }
+
     }
 }
