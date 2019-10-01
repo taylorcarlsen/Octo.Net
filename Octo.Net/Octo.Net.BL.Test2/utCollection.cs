e15e00892d763e287e785d2519068856c4061b4f
Carlsen, Taylor <taylorcarlsen13@gmail.com>
2019-10-01 17:27:12 -0500

    utCollection work with insert need to do load for the delete.

---
+++ Octo.Net/Octo.Net.BL.Test2/utCollection.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Octo.Net.BL.Test2
+{
+    [TestClass]
+    public class utCollection
+    {
+        [TestMethod]
+        public void InsertTest()
+        {
+            Models.Collection collection = new Models.Collection();
+            collection.MessageId = 9999;
+            collection.MessageTypeId = 9998;
+
+            Collection blCollection = new Collection();
+            int result = blCollection.Insert(collection);
+
+            Assert.IsTrue(result > 0);
+        }
+
+        [TestMethod]
+        public void DeleteTest()
+        {
+            Models.Collection collection = new Models.Collection();
+            List<Models.Collection> collections = new List<Models.Collection>();
+            //collections = collection.Load();
+        }
+    }
+}
