9a7efa624ddfe9ceef58337cbcf16bf19460cc43
taylorcarlsen <taylorcarlsen@users.noreply.github.com>
2019-10-08 13:47:09 -0500

    Added follower business logic

--- Octo.Net/Octo.Net.BL/Follow.cs
+++ Octo.Net/Octo.Net.BL/Follow.cs
@@ -19,5 +19,74 @@ namespace Octo.Net.BL
         {
             db.Dispose();
         }
+
+        public List<Models.Follow> Load()
+        {
+            List<Models.Follow> follows = new List<Models.Follow>();
+            db.Follows
+                .ToList()
+                .ForEach(f => follows
+                .Add(
+                    new Models.Follow
+                    {
+                        FollowerId = f.FollowerId,
+                        ArtistId = f.ArtistId
+                    }
+                    ));
+            return follows;
+        }
+
+        public List<Models.Follow> LoadByArtistId(int artistId)
+        {
+            List<Models.Follow> follows = new List<Models.Follow>();
+            db.Follows
+                .Where(f => f.Id == artistId)
+                .ToList()
+                .ForEach(f => follows
+                .Add(new Models.Follow
+                {
+                    Id = f.Id,
+                    ArtistId = f.ArtistId,
+                    FollowerId = f.FollowerId
+                }
+                    ));
+            return follows;
+                
+        }
+
+        public int Insert(Models.Follow follow)
+        {
+            tblFollow newFollow = new tblFollow { FollowerId = follow.FollowerId, ArtistId = follow.ArtistId };
+            db.Follows.Add(newFollow);
+
+            db.SaveChanges();
+            return newFollow.Id;
+        }
+        
+        public void Update(Models.Follow follow)
+        {
+            var existing = db.Follows.SingleOrDefault(f => f.Id == follow.Id);
+
+            if(existing != null)
+            {
+                existing.ArtistId = follow.ArtistId;
+                existing.FollowerId = follow.FollowerId;
+
+                db.SaveChanges();
+            }
+        }
+
+        public bool Delete(int id)
+        {
+            var existing = db.Follows.SingleOrDefault(f => f.Id == id);
+
+            if(existing != null)
+            {
+                db.Follows.Remove(existing);
+                db.SaveChanges();
+                return true;
+            }
+            return false;
+        }
     }
 }
