7a02f533e74822924aba8c62b52ac9b5521408a7
Taylor Carlsen <taylorcarlsen13@gmail.com>
2019-10-30 05:38:10 -0500

    Added the BL first items for flagging
    Related Work Items: #66, #67, #68

---
+++ Octo.Net/Octo.Net.BL/ArtworkFlag.cs
@@ -0,0 +1,78 @@
+﻿using Octo.Net.Data1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Octo.Net.BL
+{
+    public class ArtworkFlag : IDisposable
+    {
+        private readonly OctoNetDbContext db;
+
+        public ArtworkFlag()
+        {
+            db = new OctoNetDbContext();
+        }
+        public void Dispose()
+        {
+            db.Dispose();
+        }
+
+        public List<Models.ArtworkFlag> Load()
+        {
+            List<Models.ArtworkFlag> artworkFlags = new List<Models.ArtworkFlag>();
+            db.ArtworkFlags
+                .ToList()
+                .ForEach(a => artworkFlags.Add(
+                    new Models.ArtworkFlag
+                    {
+                        Id = a.Id,
+                        ArtworkId = a.ArtworkId,
+                        FlagId = a.FlagId,
+                        Comment = a.Comment
+                    }));
+
+            return artworkFlags;
+        }
+
+        public int Insert(Models.ArtworkFlag artworkFlag)
+        {
+            tblArtworkFlag newArtworkFlag = new tblArtworkFlag
+            {
+                ArtworkId = artworkFlag.ArtworkId,
+                FlagId = artworkFlag.FlagId,
+                Comment = artworkFlag.Comment
+            };
+            db.ArtworkFlags.Add(newArtworkFlag);
+            db.SaveChanges();
+            return newArtworkFlag.Id;
+        }
+
+        public void Update(Models.ArtworkFlag artworkFlag)
+        {
+            var existing = db.ArtworkFlags.SingleOrDefault(x => x.Id == artworkFlag.Id);
+
+            if(existing!=null)
+            {
+                existing.Comment = artworkFlag.Comment;
+                db.SaveChanges();
+            }
+        }
+
+        public bool Delete(int id)
+        {
+            var existing = db.ArtworkFlags.SingleOrDefault(x => x.Id == id);
+
+            if(existing!=null)
+            {
+                db.ArtworkFlags.Remove(existing);
+                db.SaveChanges();
+                return true;
+            }
+            else
+            return false;
+        }
+    }
+}
---
+++ Octo.Net/Octo.Net.BL/FlagOption.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Octo.Net.Data1;
+using Octo.Net.Models;
+
+namespace Octo.Net.BL
+{
+    public class FlagOption : IDisposable
+    {
+        private readonly OctoNetDbContext db;
+
+        public FlagOption()
+        {
+            db = new OctoNetDbContext();
+        }
+        public void Dispose()
+        {
+            db.Dispose();
+        }
+
+        public List<Models.FlagOption> Load()
+        {
+            List<Models.FlagOption> flagOptions = new List<Models.FlagOption() >;
+            db.FlagOptions
+                .ToList()
+                .ForEach(f => flagOptions
+                .Add(
+                    new Models.FlagOption
+                    {
+                        Id = f.Id,
+                        Description = f.Description
+                    }));
+
+            return flagOptions;
+        }
+    }
+}
