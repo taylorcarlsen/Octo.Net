185cb0c2c799623813c9dbbb890095962cad3ec4
brown4914 <brown4914@fvtc.edu>
2019-11-05 18:19:52 -0600

    tweaked the edit page some more

---
+++ Octo.Net/Octo.Net.UI/Content/scripts/UserCall.js
@@ -0,0 +1,12 @@
+﻿$(document).ready(function () {
+
+    var serviceURL = 'https://localhost:44332/Login/Login';
+
+    $.ajax({
+        type: "POST",
+        url: serviceURL,
+        data: param = "",
+        contentType: 
+    })
+
+})
