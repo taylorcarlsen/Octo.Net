bbac26615ebd27117caee5c28b47803c9511f46c
brown4914 <brown4914@fvtc.edu>
2019-11-10 16:22:40 -0600

    Added File Controller, file prop on user coming back as null for some reason.

---
+++ Octo.Net/Octo.Net.UI/Controllers/FileController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Octo.Net.UI.Controllers
+{
+    public class FileController : Controller
+    {
+        private readonly Octo.Net.BL.File fileContext = new BL.File();
+       
+        // GET: File
+        public ActionResult Index(int id)
+        {
+            var fileToRetrive = fileContext.LoadByArtworkId(id);
+            return File(fileToRetrive.Content, fileToRetrive.ContentType);
+        }
+    }
+}
